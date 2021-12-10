60f3668de58d89d992b58b56bd8738af3b4c7071
toddpeters <tpeters@shaw.ca>
2021-12-09 22:15:20 -0700

    Updates

---
+++ precache-manifest.bcd40c2cd9fb3129fc4773d97110d8d0.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178f4bfcb56bad4fe303dcec8d943d92",
+    "url": "/Cuadrito/my-react-web-site/index.html"
+  },
+  {
+    "revision": "a65fd081dead1e03eb4e",
+    "url": "/Cuadrito/my-react-web-site/static/css/main.f03111f2.chunk.css"
+  },
+  {
+    "revision": "e94c8ac36c5896e42c55",
+    "url": "/Cuadrito/my-react-web-site/static/js/2.712e95db.chunk.js"
+  },
+  {
+    "revision": "9c4a88d9b57cf919f2c23e04e5eabae2",
+    "url": "/Cuadrito/my-react-web-site/static/js/2.712e95db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a65fd081dead1e03eb4e",
+    "url": "/Cuadrito/my-react-web-site/static/js/main.6a1c5317.chunk.js"
+  },
+  {
+    "revision": "e9993005d572f4c9b2e4",
+    "url": "/Cuadrito/my-react-web-site/static/js/runtime-main.250d03e7.js"
+  },
+  {
+    "revision": "d70c756f2a739722a398819cfa7287a7",
+    "url": "/Cuadrito/my-react-web-site/static/media/bitcoin.d70c756f.png"
+  },
+  {
+    "revision": "01899307116a937c66a0eb4b3927b631",
+    "url": "/Cuadrito/my-react-web-site/static/media/blob.01899307.png"
+  },
+  {
+    "revision": "8d581ffb03c56a0106abc6b330b449d5",
+    "url": "/Cuadrito/my-react-web-site/static/media/cercuri.8d581ffb.png"
+  },
+  {
+    "revision": "62a04e2688d8ba7ede0d823e425cf999",
+    "url": "/Cuadrito/my-react-web-site/static/media/chester-wade.62a04e26.jpg"
+  },
+  {
+    "revision": "2ae3f9ecc812ca146d952de9c8e8c0ba",
+    "url": "/Cuadrito/my-react-web-site/static/media/denys.2ae3f9ec.jpg"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/Cuadrito/my-react-web-site/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "4fd59ead57f5b7dfa3ca0db01414a256",
+    "url": "/Cuadrito/my-react-web-site/static/media/etherum.4fd59ead.png"
+  },
+  {
+    "revision": "182796a27d309b897e77f616df128522",
+    "url": "/Cuadrito/my-react-web-site/static/media/fabien-bazanegue.182796a2.jpg"
+  },
+  {
+    "revision": "2e2571256140872fb7e4f246ea786669",
+    "url": "/Cuadrito/my-react-web-site/static/media/github.2e257125.svg"
+  },
+  {
+    "revision": "08ea438ed3b14e63dd54516d3768e3d3",
+    "url": "/Cuadrito/my-react-web-site/static/media/google.08ea438e.svg"
+  },
+  {
+    "revision": "3a749d183532590fda1c14345b6ccca6",
+    "url": "/Cuadrito/my-react-web-site/static/media/mark-finn.3a749d18.jpg"
+  },
+  {
+    "revision": "be4a1748d0f94c35e0a661164495d160",
+    "url": "/Cuadrito/my-react-web-site/static/media/mike.be4a1748.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/Cuadrito/my-react-web-site/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/Cuadrito/my-react-web-site/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/Cuadrito/my-react-web-site/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/Cuadrito/my-react-web-site/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "43ff45c5dc1f65726b86fc75ea6cabc0",
+    "url": "/Cuadrito/my-react-web-site/static/media/path2.43ff45c5.png"
+  },
+  {
+    "revision": "a70167ea1491987c50049961a9e2e45f",
+    "url": "/Cuadrito/my-react-web-site/static/media/path3.a70167ea.png"
+  },
+  {
+    "revision": "e37326c7e0fd9826c1a35b231340f58b",
+    "url": "/Cuadrito/my-react-web-site/static/media/path4.e37326c7.png"
+  },
+  {
+    "revision": "ceb241f05718fb1311f8810eea959803",
+    "url": "/Cuadrito/my-react-web-site/static/media/path5.ceb241f0.png"
+  },
+  {
+    "revision": "03ea2a0ad05689cebb5a9314a53b8966",
+    "url": "/Cuadrito/my-react-web-site/static/media/patrat.03ea2a0a.png"
+  },
+  {
+    "revision": "98742a820d2986e78ae284d57deae457",
+    "url": "/Cuadrito/my-react-web-site/static/media/ripp.98742a82.png"
+  },
+  {
+    "revision": "79c1998fb18c2d0078bee11f902364e3",
+    "url": "/Cuadrito/my-react-web-site/static/media/square-purple-1.79c1998f.png"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/Cuadrito/my-react-web-site/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/Cuadrito/my-react-web-site/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/Cuadrito/my-react-web-site/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/Cuadrito/my-react-web-site/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/Cuadrito/my-react-web-site/static/media/square5.ff004a0e.png"
+  },
+  {
+    "revision": "11fa5f73cb719f0f1a38e02e47bdc843",
+    "url": "/Cuadrito/my-react-web-site/static/media/waves.11fa5f73.png"
+  }
+]);
